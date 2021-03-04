951717aad9a42b8e3d6bd8aaeef12747abc05a31
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-03-04 09:47:48 -0800

    refactored best of my abilities

--- tic_toc_toe_messy.py
+++ tic_toc_toe_messy.py
@@ -1,18 +1,10 @@
-# Tic Tac Toe
-# Reference: With modification from http://inventwithpython.com/chapter10.html. 
-
-# TODOs:  
-# 1. Find all TODO items and see whether you can improve the code. 
-#    In most cases (if not all), you can make them more readable/modular.
-# 2. Add/fix function's docstrings (use """ insted of # for function's header
-#    comments)
-
 import random
 
 BOARD_SPACE = 10
 
 def draw_board(board):
     ''' This function prints out board'''
+   
     print('   |   |')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
@@ -27,7 +19,9 @@ def draw_board(board):
 
 def input_player_letter():
     ''' lets player input and returns list with player’s and computers letter'''
+   
     letter = ''
+  
     while not (letter == 'X' or letter == 'O'):
         print('Do you want to be X or O?')
         letter = input().upper()
@@ -39,6 +33,7 @@ def input_player_letter():
 
 def who_goes_first():
     """This function randomly chooses player or computer to go first"""
+   
     if random.randint(0, 1) == 0:
         return 'computer'
     else:
@@ -46,15 +41,18 @@ def who_goes_first():
 
 def play_again():
     ''' ask player to play again'''
+    
     print('Do you want to play again? (yes or no)')
     return input().lower().startswith('y')
 
 def make_move(board, letter, move):
     '''placers letter on board'''
+   
     board[move] = letter
 
 def is_winner(bo, le):
     ''' Given a board and a player’s letter, this function returns True if that player has won. '''
+   
     return ((bo[7] == le and bo[8] == le and bo[9] == le) or # across the top
             (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle    
             (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
@@ -65,6 +63,7 @@ def is_winner(bo, le):
 
 def get_board_copy(board):
     ''' Make a duplicate of the board list and return it the duplicate. '''
+    
     dupe_board= []
     for i in board:
         dupe_board.append(i)
@@ -72,11 +71,14 @@ def get_board_copy(board):
 
 def is_space_free(board, move):
     '''Return true if the passed move is free on the passed board.'''
+    
     return board[move] == ' '
 
 def get_player_move(board):
     ''' Let the player type in their move.'''
+    
     player_move = ' '
+    
     while player_move not in '1 2 3 4 5 6 7 8 9'.split() or not is_space_free(board, int(player_move)):
         print('What is your next move? (1-9)')
         player_move = input()
@@ -84,7 +86,9 @@ def get_player_move(board):
 
 def choose_random_move_from_list(board, moves_list):
     '''Returns a valid move from the passed list on the passed and return none if not valid '''
+   
     possible_moves = []
+    
     for i in moves_list:
         if is_space_free(board, i):
             possible_moves.append(i)
@@ -95,6 +99,7 @@ def choose_random_move_from_list(board, moves_list):
 
 def get_computer_move(board, computer_letter): 
     ''' Given a board and the computer's letter, determine where to move and return that move.'''
+    
     if computer_letter == 'X':
         player_letter = 'O'
     else:
@@ -130,60 +135,60 @@ def get_computer_move(board, computer_letter):
 
 def is_board_full(board):
     '''Return True if every space on the board has been taken. Otherwise return False.'''
+   
     for i in range(1, BOARD_SPACE):
         if is_space_free(board, i):
             return False
     return True
 
 def start_game():
+    """starts the tic tac toe game"""
+   
     print('Welcome to Tic Tac Toe!')
-
+    
     while True:
-        # Reset the board
-        theBoard = [' '] * 10 # TODO: Refactor the magic number in this line (and all of the occurrences of 10 thare are conceptually the same.)
+        the_board = [' '] * BOARD_SPACE
         player_letter, computer_letter = input_player_letter()
         turn = who_goes_first()
         print('The ' + turn + ' will go first.')
-        gameIsPlaying = True # TODO: Study how this variable is used. Does it ring a bell? (which refactoring method?) 
-                            #       See whether you can get rid of this 'flag' variable. If so, remove it.
 
-        while gameIsPlaying: # TODO: Usually (not always), loops (or their content) are good candidates to be extracted into their own function.
-                            #       Use a meaningful name for the function you choose.
-            if turn == 'player':
-                # Player’s turn.
-                draw_board(theBoard)
-                move = get_player_move(theBoard)
-                make_move(theBoard, player_letter, move)
+        while True:
+                if turn == 'player':
 
-                if is_winner(theBoard, player_letter):
-                    draw_board(theBoard)
-                    print('Hooray! You have won the game!')
-                    gameIsPlaying = False
-                else:  # TODO: is this 'else' necessary?
-                    if is_board_full(theBoard):
-                        draw_board(theBoard)
+                    draw_board(the_board)
+                    move = get_player_move(the_board)
+                    make_move(the_board, player_letter, move)
+                    # player wins
+                    if is_winner(the_board, player_letter):
+                        draw_board(the_board)
+                        print('Hooray! You have won the game!')
+                        break
+
+                    if is_board_full(the_board):
+                        draw_board(the_board)
                         print('The game is a tie!')
                         break
-                    else:  # TODO: Is this 'else' necessary?
-                        turn = 'computer'
+                    turn = 'computer'
 
-            else:
-                # Computer’s turn.
-                move = get_computer_move(theBoard, computer_letter)
-                make_move(theBoard, computer_letter, move)
-
-                if is_winner(theBoard, computer_letter):
-                    draw_board(theBoard)
-                    print('The computer has beaten you! You lose.')
-                    gameIsPlaying = False
-                else:     # TODO: is this 'else' necessary?
-                    if is_board_full(theBoard):
-                        draw_board(theBoard)
+                else:
+                    move = get_computer_move(the_board, computer_letter)
+                    make_move(the_board, computer_letter, move)
+                    # computer wins
+                    if is_winner(the_board, computer_letter):
+                        draw_board(the_board)
+                        print('The computer has beaten you! You lose.')
+                        break
+                    # game is tie
+                    if is_board_full(the_board):
+                        draw_board(the_board)
                         print('The game is a tie!')
                         break
-                    else: # TODO: Is this 'else' necessary?
-                        turn = 'player'
+                    turn = 'player'
 
         if not play_again():
             break
+
 start_game()
+
+
+
